3e29950a0c1ad4dbadae804433c08bbffa84b9ed
Anthony Dickson <dican732@student.otago.ac.nz>
2021-10-12 10:09:03 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebXR 360 Degree Video Demo
+## Getting Started
+```shell
+git clone https://github.com/eight0153/depth-from-mono-demo
+cd depth-from-mono-demo
+npm install
+npm start
+```
+
+## Updating the GitHub Page
+1. Run the following commands after updating the page on the main branch.
+    ```shell
+    git checkout gh-pages # Change to the branch that GitHub uses
+    git merge master # Copy changes from the main branch. 
+    npm build
+    # Built files must be copied to the dir that GitHub is setup to use (I use root/ in this example).
+    cp -r dist/* .
+    # Add the new javascript files
+    git add index* 
+    # (Optional) if you added video
+    git add video/*
+    git commit -m "Your commit message here"
+    git push
+    ```
+2. Reload \<GitHub Username>.github.io/\<Repository name> and it should show the updated contents.
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import Stats from "three/examples/jsm/libs/stats.module.js";
+
+
+window.onload = () => {
+    init();
+}
+
+function init() {
+    const displacementScale = -10.0;
+    const displacementBias = -1.0;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer();
+    // TODO: Find fix for video textures pausing if source video is not visible.
+    // TODO: Remove scaling from renderer initialiser.
+    renderer.setSize(window.innerWidth * 0.75, window.innerHeight * 0.75);
+    document.body.appendChild(renderer.domElement)
+
+    const stats = Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(stats.dom);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    scene.add(new THREE.AmbientLight(0xffffff));
+
+    const geometry = new THREE.PlaneGeometry(6.4, 4.8, 320, 240);
+
+    const rgbVideo = document.getElementById("rgb-video") as HTMLVideoElement
+    const rgbTexture = new THREE.VideoTexture(rgbVideo)
+
+    const depthVideo = document.getElementById("depth-video") as HTMLVideoElement
+    const depthTexture = new THREE.VideoTexture(depthVideo)
+
+    const bgAlphaVideo = document.getElementById("bg-alpha-video") as HTMLVideoElement
+    const bgAlphaTexture = new THREE.VideoTexture(bgAlphaVideo)
+
+    const backgroundMaterial = new THREE.MeshPhongMaterial({
+        map: rgbTexture, displacementMap: depthTexture,
+        alphaMap: bgAlphaTexture, transparent: true,
+        displacementScale: displacementScale, displacementBias: displacementBias, flatShading: true, toneMapped: false
+    })
+    const backgroundMesh = new THREE.Mesh(geometry, backgroundMaterial);
+    scene.add(backgroundMesh);
+
+    const object1Video = document.getElementById("object-001-video") as HTMLVideoElement
+    const object1Texture = new THREE.VideoTexture(object1Video)
+
+    const foregroundMaterial = new THREE.MeshPhongMaterial({
+        map: rgbTexture, displacementMap: depthTexture,
+        alphaMap: object1Texture, transparent: true,
+        displacementScale: displacementScale, displacementBias: displacementBias, flatShading: true, toneMapped: false
+    })
+    const foregroundMesh = new THREE.Mesh(geometry, foregroundMaterial);
+    scene.add(foregroundMesh);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+        requestAnimationFrame(animate);
+
+        stats.begin();
+
+        renderer.render(scene, camera);
+
+        stats.end()
+    };
+
+    animate();
+}
