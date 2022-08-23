d520ec3f1c1d78951182f14249798cdcabe20d63
Jeremy Shanks <shaje415@student.otago.ac.nz>
2022-08-23 16:00:38 +1200

    Added interactive buttons to control playback
    Large overhaul primarily adding interactive buttons to enable the user to control playback. The buttons can be interacted with by using a mouse or a VR controller. The button logic uses the clock object to start and stop video playback.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 // @ts-ignore
 import * as THREE from 'three'
 // @ts-ignore
+import ThreeMeshUI from 'three-mesh-ui';
+// @ts-ignore
 import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls.js'
+// @ts-ignore
+import VRControl from './utils/VRControl.js';
+// @ts-ignore
+import {HoveredButton, IdleButton, SelectedButtonAttributes, ButtonOptions} from './UIConstants';
 // @ts-ignore
 import Stats from "three/examples/jsm/libs/stats.module.js"
 // @ts-ignore
@@ -9,6 +15,50 @@ import {VRButton} from 'three/examples/jsm/webxr/VRButton.js'
 // @ts-ignore
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
+// global variables
+let vrControl, camera, renderer;
+let clock = new THREE.Clock();
+let selectState = false;
+let touchState = false;
+const raycaster = new THREE.Raycaster();
+const objsToTest = [];
+
+// set up the mouse
+const mouse = new THREE.Vector2();
+mouse.x = mouse.y = null;
+
+window.addEventListener( 'pointermove', ( event ) => {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
+} );
+
+window.addEventListener( 'pointerdown', () => {
+	selectState = true;
+} );
+
+window.addEventListener( 'pointerup', () => {
+	selectState = false;
+} );
+
+window.addEventListener( 'resize', onWindowResize );
+
+window.addEventListener( 'touchstart', ( event ) => {
+
+	touchState = true;
+	mouse.x = ( event.touches[ 0 ].clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = -( event.touches[ 0 ].clientY / window.innerHeight ) * 2 + 1;
+
+} );
+
+window.addEventListener( 'touchend', () => {
+
+	touchState = false;
+	mouse.x = null;
+	mouse.y = null;
+
+} );
+
+// when the window loads this calls the init() function
 window.onload = () => {
     init()
 }
@@ -23,9 +73,11 @@ class MeshVideo {
     private readonly persistFrame: boolean
     private readonly swapMeshInterval: number
     private timeSinceLastMeshSwap: number
-    // The current frame position in the video sequence.
+
+    /** The current frame position in the video sequence. */
     private currentFrameIndex: number
-    // The index of the currently displayed frame (null if no frame is currently displayed).
+
+    /** The index of the currently displayed frame (null if no frame is currently displayed). */
     private displayedFrameIndex: number
 
     public numFrames: number
@@ -61,12 +113,23 @@ class MeshVideo {
         this.displayedFrameIndex = null
     }
 
+    // 
+    clear() {
+        this.currentFrameIndex = 0
+    }
+
+	// Returns the current frame index.
+	getCurrentFrameIndex(): number {
+		return this.currentFrameIndex;
+	}
+
     /**
      * Load the mesh data from disk.
      * @return A reference to this MeshVideo object.
      *  Allows a call to this load function to be chained when creating a new instance.
      */
     load(): MeshVideo {
+
         this.loader.load(
             `${this.videoBaseFolder}/${this.sceneName}.glb`,
             (gltf) => {
@@ -190,7 +253,9 @@ const createRenderer = (width: number, height: number): THREE.WebGLRenderer => {
     renderer.setClearColor(0x000000, 1)
     renderer.xr.enabled = true
     renderer.xr.setReferenceSpaceType('local')
+
     document.body.appendChild(VRButton.createButton(renderer))
+	document.body.appendChild( renderer.domElement )
 
     return renderer
 }
@@ -252,22 +317,246 @@ const loadSkybox = (): THREE.CubeTexture => {
         ])
 }
 
+function createButton(name: string = null, imageURL: string = null, func: any): any {
+    
+    const button = new ThreeMeshUI.Block(ButtonOptions);
+
+    if (imageURL) {
+        new THREE.TextureLoader().load(imageURL, (texture) => {
+            button.set({
+              backgroundTexture: texture,
+            });
+        });
+    }
+
+    if (name) {
+        button.add(
+            new ThreeMeshUI.Text({content: name})
+        );
+    }
+
+    button.setupState({
+        state: "selected",
+        attributes: SelectedButtonAttributes,
+        onSet: func
+    });
+
+    button.setupState(HoveredButton);
+    button.setupState(IdleButton);
+
+    return button;
+}
+
+function createContainer(position: THREE.Vector3, xrot: number = -0.55) {
+    const container = <THREE.Object3D>new ThreeMeshUI.Block({
+        justifyContent: 'center',
+        alignContent: 'center',
+        contentDirection: 'row-reverse',
+        fontFamily: './assets/Roboto-msdf.json',
+        fontTexture: './assets/Roboto-msdf.png',
+        fontSize: 0.07,
+        padding: 0.02,
+        borderRadius: 0.11
+    });
+    container.position.set(position.x, position.y, position.z);
+    container.rotation.x = xrot;
+    container.name = "ButtonContainer";
+    return container;
+}
+
+// Called in the loop, get intersection with either the mouse or the VR controllers,
+// then update the buttons states according to result
+
+function updateButtons() {
+
+	// Find closest intersecting object
+
+	let intersect;
+
+	if ( renderer.xr.isPresenting ) {
+
+		vrControl.setFromController( 0, raycaster.ray );
+
+		intersect = raycast();
+
+		// Position the little white dot at the end of the controller pointing ray
+		if ( intersect ) vrControl.setPointerAt( 0, intersect.point );
+
+	} else if ( mouse.x !== null && mouse.y !== null ) {
+
+		raycaster.setFromCamera( mouse, camera );
+
+		intersect = raycast();
+
+	}
+
+	// Update targeted button state (if any)
+
+	if ( intersect && intersect.object.isUI ) {
+
+		if ( selectState ) {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			intersect.object.setState( 'selected' );
+
+		} else {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			intersect.object.setState( 'hovered' );
+
+		}
+
+	}
+
+	// Update non-targeted buttons state
+
+	objsToTest.forEach( ( obj ) => {
+
+		if ( ( !intersect || obj !== intersect.object ) && obj.isUI ) {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			obj.setState( 'idle' );
+
+		}
+
+	} );
+
+}
+
+// calculates rays
+function raycast() {
+
+	return objsToTest.reduce( ( closestIntersection, obj ) => {
+
+		const intersection = raycaster.intersectObject( obj, true );
+
+		if ( !intersection[ 0 ] ) return closestIntersection;
+
+		if ( !closestIntersection || intersection[ 0 ].distance < closestIntersection.distance ) {
+
+			intersection[ 0 ].object = obj;
+
+			return intersection[ 0 ];
+
+		}
+
+		return closestIntersection;
+
+	}, null );
+
+}
+
+// pause button. if the video is currently playing, pause the playback, if 
+// the video is currently paused, resume playback.
+function buttonPause(){
+    console.log("the pause button has been clicked");
+
+    if(clock.running) {
+        clock.stop();
+    } else {
+        clock.start();
+    }
+}
+
+// stop button. pauses the foreground and background animation and resets
+// the current frame to the first frame in the sequence.
+function buttonStop(){
+    console.log("the stop button has been clicked");
+	clock.stop();
+
+    // TODO:
+    // is the stop button missing functionality? should the
+    // stop button reset the current animation back to the beginning?
+}
+
+// play button. if the animation is paused, resume the playback, otherwise
+// do nothing!
+function buttonPlay(){
+    console.log("the play button has been clicked");
+    clock.start();
+}
+
+// start button. resets the current frame to the first frame in the sequence
+// and plays
+function buttonStart(){
+    console.log("the start button has been clicked");
+    clock.start();
+}
+
+// onWindowResize will be called when the window is resized
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
 function init() {
     const canvasWidth = window.innerWidth
     const canvasHeight = window.innerHeight
     const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 1000)
-    const renderer = createRenderer(canvasWidth, canvasHeight)
+    camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 1000)
+    renderer = createRenderer(canvasWidth, canvasHeight)
     const controls = createControls(camera, renderer)
     const stats = createStatsPanel()
 
     const resetCamera = () => {
+
+        // the trackball controls will always be centered on (0, 0, 0)
+
         controls.reset()
-        camera.position.z = -1.5
-        camera.lookAt(0, 0, 0)
-        // Have to move the world instead of the camera to get the controls to behave correctly...
+        camera.position.set(0, 0, -1)
+
+        // this means we must move the scene instead of the camera to get the controls
+        // to work with the center
         scene.position.y = -1.5
     }
+
+    // initialise VR controllers
+    vrControl = VRControl( renderer, camera, scene );
+
+    const controllers = new THREE.Group();
+
+    controllers.add( vrControl.controllerGrips[ 0 ], vrControl.controllers[ 0 ] );
+
+    vrControl.controllers[ 0 ].addEventListener( 'selectstart', () => {
+
+        selectState = true;
+
+    } );
+    vrControl.controllers[ 0 ].addEventListener( 'selectend', () => {
+
+        selectState = false;
+
+    } );
+
+    controllers.add( vrControl.controllerGrips[ 1 ], vrControl.controllers[ 1 ] );
+
+    vrControl.controllers[ 1 ].addEventListener( 'selectstart', () => {
+
+        selectState = true;
+
+    } );
+    vrControl.controllers[ 1 ].addEventListener( 'selectend', () => {
+
+        selectState = false;
+
+    } );
+
+    // since we move the scene to be "centered" on the trackball controller,
+    // we need to move the controllers to match the new scene location
+    controllers.translateY(1.5);
+
+    scene.add(controllers);
+    // end of initialising VR controllers
+
+    // setup the VR camera if in VR
+    //
+    // the VR camera by default starts in a strange (x, y, z) 
+    // and faces in the wrong direction (needs rotation towards the objects)
+    //renderer.xr.getCamera().cameras[0].position.x = 10;
+    //renderer.render( scene, renderer.xr.getCamera().cameras[0] );
 
     const onDocumentKeyDown = (event) => {
         const keyCode = event.which;
@@ -276,7 +565,7 @@ function init() {
                 resetCamera()
                 break
             }
-            case 80: {
+            case 80: { // the key 'p'
                 console.info(`Camera position: (${camera.position.x}, ${camera.position.y}, ${camera.position.z})`)
                 console.info(`Camera rotation: (${camera.rotation.x}, ${camera.rotation.y}, ${camera.rotation.z})`)
                 let cameraDirection = new THREE.Vector3()
@@ -290,6 +579,8 @@ function init() {
     }
     document.addEventListener("keydown", onDocumentKeyDown, false);
 
+    // TODO:
+    // add catalogue functionality here
     const videoFolder = getVideoFolder()
     document.title = `3D Video | ${videoFolder}`
 
@@ -300,6 +591,7 @@ function init() {
         const loader = new GLTFLoader()
         const swapMeshInterval = 1.0 / metadata["fps"] // seconds
 
+        // loads the dynamic elements aka foreground
         const dynamicElements = new MeshVideo({
             swapMeshInterval,
             loader,
@@ -309,6 +601,7 @@ function init() {
             persistFrame: false
         }).load()
 
+        // loads the static elements aka background
         const staticElements = new MeshVideo({
             swapMeshInterval,
             loader,
@@ -318,16 +611,38 @@ function init() {
             persistFrame: true
         }).load()
 
+        // add white plane as floor
         scene.add(getGroundPlane(100, 100))
+
+        // add nice clouds as background
         scene.background = loadSkybox()
 
-        const clock = new THREE.Clock()
+        // setup the button panel
+		let buttonContainer = createContainer(new THREE.Vector3(0, 1, 0));
+        
+        let buttons = [
+			createButton(null, "./assets/pause.png", () => {buttonPause()}),
+            createButton(null, "./assets/stop.png", () => {buttonStop()}),
+            createButton(null, "./assets/play.png", () => {buttonPlay()}),
+            createButton(null, "./assets/start.png", () => {buttonStart(), dynamicElements.clear()})
+		];
+		buttons.forEach(button => buttonContainer.add(button));
+		buttons.forEach(button => objsToTest.push(button));
+
+        let buttonGroup = new THREE.Group();
+        buttonGroup.add(buttonContainer);
+        buttonContainer.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), THREE.MathUtils.DEG2RAD * -180);
+
+        scene.add(buttonGroup);
 
         renderer.setAnimationLoop(() => {
             stats.begin()
 
+            ThreeMeshUI.update()
+
+            // loading block
             if (loadingOverlay.isVisible && dynamicElements.hasLoaded && staticElements.hasLoaded) {
-                // Ensure that the two clips will be synced
+                // Ensure that the two clips (fg and bg) will be synced
                 const numFrames = Math.max(staticElements.numFrames, dynamicElements.numFrames)
                 dynamicElements.numFrames = numFrames
                 staticElements.numFrames = numFrames
@@ -339,15 +654,24 @@ function init() {
                 loadingOverlay.hide()
 
                 clock.start()
+
+				// render the scene while still being paused
+				dynamicElements.update(0.25, scene)
+                staticElements.update(0.25, scene)
+
+                clock.stop()
             }
 
             const delta = clock.getDelta()
 
-            dynamicElements.update(delta, scene)
+			dynamicElements.update(delta, scene)
             staticElements.update(delta, scene)
+            
             controls.update()
 
             renderer.render(scene, camera)
+
+			updateButtons()
 
             stats.end()
         })
