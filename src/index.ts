350b36218175383d4a73dc388de1d085652783c5
Jeremy Shanks <shaje415@student.otago.ac.nz>
2022-09-18 15:45:22 +1200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -896,6 +896,28 @@ function init() {
         
         let isXRCameraFixed = false;
 
+        // test of directory thing
+        //
+        //requiring path and fs modules
+        const path = require('path');
+        const fs = require('fs');
+        //joining path of directory 
+        const directoryPath = path.join(__dirname, 'Documents');
+        //passsing directoryPath and callback function
+        fs.readdir(directoryPath, function (err, files) {
+            //handling error
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            //listing all files using forEach
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                console.log(file); 
+            });
+        });
+        //
+        // test of directory thing
+
         renderer.setAnimationLoop(() => {
 
             // displays the statistics in the top left corner
